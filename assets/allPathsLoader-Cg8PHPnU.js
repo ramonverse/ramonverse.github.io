9611bc64515d0b121d391945e5ccc065b0865db1
Ramon Garate <rg@rgs-macbook-air.lan>
2024-03-20 20:09:36 -0700

    update pyodide

--- assets/allPathsLoader-Cg8PHPnU.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-G-AC-0r6.js","assets/index-BIdTI090.js","assets/index-ClTF5qbq.js","assets/index-C7c1b9ND.js","assets/index-Bx6Jj-Nm.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as s,a as o,b as i,__tla as l}from"./index-C7c1b9ND.js";let r,c=Promise.all([(()=>{try{return l}catch{}})()]).then(async()=>{r=function(e,_){return s(void 0,void 0,void 0,function(){var t;return o(this,function(a){switch(a.label){case 0:return[4,i(()=>import("./allPaths-G-AC-0r6.js").then(async n=>(await n.__tla,n)),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=a.sent().getIconPaths,[2,t(e,_)]}})})}});export{c as __tla,r as allPathsLoader};
