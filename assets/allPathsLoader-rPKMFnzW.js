5258950c40f4855f3e957b34f23e8565b479ef2d
Ramon Garate <rg@rgs-macbook-air.lan>
2024-03-24 17:36:26 -0700

    remove console.log

--- assets/allPathsLoader-rPKMFnzW.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-BgX8gK_x.js","assets/index-BIdTI090.js","assets/index-ClTF5qbq.js","assets/index-8sHwAL2U.js","assets/index-DHIuKjZq.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as s,a as o,b as i,__tla as l}from"./index-8sHwAL2U.js";let r,c=Promise.all([(()=>{try{return l}catch{}})()]).then(async()=>{r=function(e,_){return s(void 0,void 0,void 0,function(){var t;return o(this,function(a){switch(a.label){case 0:return[4,i(()=>import("./allPaths-BgX8gK_x.js").then(async n=>(await n.__tla,n)),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=a.sent().getIconPaths,[2,t(e,_)]}})})}});export{c as __tla,r as allPathsLoader};
